f44cd6c69d9dc4ef51e33c88c81b742a7e522d5d
Adam Derylo <aderylo@noreply>
2024-11-13 16:00:18 +0100

    1.6. Rigid Shape Aligment with Procrustes

--- exercise1/exercise_1/alignment.py
+++ exercise1/exercise_1/alignment.py
@@ -13,13 +13,25 @@ def procrustes_align(pc_x, pc_y):
     R = np.zeros((3, 3), dtype=np.float32)
     t = np.zeros((3,), dtype=np.float32)
 
-    # TODO: Your implementation starts here ###############
     # 1. get centered pc_x and centered pc_y
+    mean_x = np.mean(pc_x, axis=0)
+    mean_y = np.mean(pc_y, axis=0)
+    centered_x = pc_x - mean_x
+    centered_y = pc_y - mean_y
     # 2. create X and Y both of shape 3XN by reshaping centered pc_x, centered pc_y
+    X = centered_x.T  
+    Y = centered_y.T  
     # 3. estimate rotation
+    covariance_matrix = np.dot(X, Y.T)
+    U, _, Vt = np.linalg.svd(covariance_matrix)
+    R = np.dot(Vt.T, U.T)
+    # rotation (det = 1) instead of reflection (det = -1) 
+    if np.linalg.det(R) < 0:  
+        Vt[-1, :] *= -1
+        R = np.dot(Vt.T, U.T)
     # 4. estimate translation
+    t = mean_y - np.dot(R, mean_x)
     # R and t should now contain the rotation (shape 3x3) and translation (shape 3,)
-    # TODO: Your implementation ends here ###############
 
     t_broadcast = np.broadcast_to(t[:, np.newaxis], (3, pc_x.shape[0]))
     print('Procrustes Aligment Loss: ', np.abs((np.matmul(R, pc_x.T) + t_broadcast) - pc_y.T).mean())
